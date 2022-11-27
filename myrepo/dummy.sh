fdc1cb3a442850087f8492d3f48f4e8caa03809e
Ebenneil <106720996+Ebenneil@users.noreply.github.com>
2022-11-27 19:58:28 +0530

    Update dummy.sh

--- myrepo/dummy.sh
+++ myrepo/dummy.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-apt update
-apt install deafult-jre
-apt install maven
-cd /opt
-wget https://downloads.apache.org/tomcat/tomcat-10/v10.0.27/bin/apache-tomcat-10.0.27.tar.gz
-tar -xzvf apache-tomcat-10.0.27.tar.gz 
-cd apache-tomcat-10.0.27
-vi conf/tomcat-users.xml
-vi webapps/manager/META-INF/context.xml
-vi webapps/host-manager/META-INF/context.xml
-cd ../../
-git clone https://github.com/Yetish519/hello-world-war.git 
-cd hello-world-war
-mvn package
-cd target
-cp -r hello-world-war-1.0.0.war /opt/apache-tomccat
+sudo apt update -y
+sudo apt install default-jdk -y
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.27/bin/apache-tomcat-10.0.27.tar.gz
+sudo tar -xvzf apache-tomcat-10.0.27.tar.gz
+sudo rm -rf apache-tomcat-10.0.27.tar.gz
+sudo mv apache-tomcat-10.0.27 /opt/tomcat
+sudo sh /opt/tomcat/bin/startup.sh
+sudo rm -rf conf-and-webapps-file
+sudo git clone https://github.com/syedwaliuddin/conf-and-webapps-file.git
+sudo rm -rf /opt/tomcat/conf/tomcat-users.xml
+sudo cp conf-and-webapps-file/tomcat-users.xml /opt/tomcat/conf/
+sudo sh /opt/tomcat/bin/startup.sh
+sudo rm -rf /opt/tomcat/webapps/manager/META-INF/context.xml
+sudo cp conf-and-webapps-file/context.xml /opt/tomcat/webapps/manager/META-INF/
+sudp rm -rf /opt/tomcat/webapps/host-manager/META-INF/context.xml
+sudo cp conf-and-webapps-file/contexthm.xml /opt/tomcat/webapps/host-manager/META-INF/
+sudo sh /opt/tomcat/bin/startup.sh
+sudo rm -rf conf-and-webapps-file
+sudo rm -rf apache-tomcat-10.0.27
