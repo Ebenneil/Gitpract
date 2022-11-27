2bbf193638f2aa34c67edad28e83b09f5a063432
Ebenneil <106720996+Ebenneil@users.noreply.github.com>
2022-11-27 13:40:09 +0530

    Update 2int.sh

--- myrepo/2int.sh
+++ myrepo/2int.sh
@@ -3,5 +3,5 @@ echo " enter the first int value "
 read a 
 echo "enter the second int value"
 read b
-echo "first int value is $a"
-echo "second int value is $b"
+echo "the first int value is $a"
+echo "the second int value is $b"
