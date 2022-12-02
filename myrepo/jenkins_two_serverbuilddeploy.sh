0511cd5fa09cd060853c3704ab590ab90d89d47a
Ebenneil <106720996+Ebenneil@users.noreply.github.com>
2022-12-02 14:51:39 +0530

    Create jenkins_two_serverbuilddeploy.sh

---
+++ myrepo/jenkins_two_serverbuilddeploy.sh
@@ -0,0 +1,24 @@
+pipeline {
+	agent none
+        stages {
+           stage ("tomcat buid & move to other node") {
+	       agent {label "tom"}
+              steps {
+                      sh 'sudo mvn package'
+		      sh 'ls'
+		      sh 'scp -R target/hello-world-war-1.0.0.war banglore@172.31.34.151:/opt/tomcat/webapps'
+		      echo "sucessfully copied build to other node"
+	      }
+	   }
+	   stage ('diploy in node2') {
+	      agent {label "banglore"}
+	   	steps {
+		    sh 'sudo sh /opt/tomcat/bin/shutdown.sh'                   
+                    sh 'sudo sleep 3'
+                    sh 'sudo sh /opt/tomcat/bin/startup.sh'
+                    echo "diployment is sucessfull"
+                    echo "copy the public ip of instace and open it in browser with port:8090"
+		}
+	}
+	} 	   
+        }
